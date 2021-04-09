8d57c4c8bddba66b94b73716e03f108189c30011
Ivan Drozdovskiy <vania110297@gmail.com>
2021-04-09 14:16:26 +0300

    feat: add solutions for common-character and mine-sweeper

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,17 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let str2 = s2;
+  for (let i = 0; i < s1.length; i++) {
+    const index = str2.search(s1.charAt(i));
+    if (index !== -1) {
+      count++;
+      str2 = str2.replace(str2.charAt(index), '');
+    }
+  }
+  return count;
 }
 
 module.exports = getCommonCharacterCount;
--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,41 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = matrix.map((el) => el.slice());
+  res.map((el) => el.fill(0));
+  const width = matrix[0].length;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < width; j++) {
+      if (matrix[i][j] === true) {
+        if (res[i - 1]) {
+          if (j !== 0) {
+            res[i - 1][j - 1]++;
+          }
+          if (j !== width) {
+            res[i - 1][j + 1]++;
+          }
+          res[i - 1][j]++;
+        }
+        if (j !== 0) {
+          res[i][j - 1]++;
+        }
+        if (j !== width) {
+          res[i][j + 1]++;
+        }
+        if (res[i + 1]) {
+          if (j !== 0) {
+            res[i + 1][j - 1]++;
+          }
+          if (j !== width) {
+            res[i + 1][j + 1]++;
+          }
+          res[i + 1][j]++;
+        }
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = minesweeper;
