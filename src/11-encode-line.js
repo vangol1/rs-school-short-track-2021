bd866b5c3b5350028e8d6de4bd244408843633a3
Ivan Drozdovskiy <vania110297@gmail.com>
2021-04-11 22:56:57 +0300

    feat: add solutions for find index and encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let current = arr[0];
+  let count = 0;
+  let res = '';
+  arr.forEach((elem, index) => {
+    if (elem !== current) {
+      if (count > 1) {
+        res += `${count}${current}`;
+      } else {
+        res += current.toString();
+      }
+      count = 1;
+      current = elem;
+    } else {
+      count++;
+    }
+    if (index === arr.length - 1) {
+      if (count > 1) {
+        res += `${count}${current}`;
+      } else {
+        res += current.toString();
+      }
+    }
+  });
+  return res;
 }
 
 module.exports = encodeLine;
